1f81613e63ad52ee42e570c8726a16bf3471d709
jacem1 <mohamed.jacem@gmail.com>
2025-12-04 14:51:44 +0100

    Add cardData array with sample credit card info

---
+++ cards.js
@@ -0,0 +1,72 @@
+const cardData = [
+    {
+        "type": "Visa",
+        "name": "James Reed",
+        "number": "4195462320425898",
+        "cvv": "903",
+        "expiry": "12/27"
+    },
+    {
+        "type": "JCB",
+        "name": "Riley Miller",
+        "number": "3523272171732646",
+        "cvv": "696",
+        "expiry": "12/27"
+    },
+    {
+        "type": "Visa",
+        "name": "Hannah Clark",
+        "number": "4967782302263002",
+        "cvv": "773",
+        "expiry": "12/29"
+    },
+    {
+        "type": "JCB",
+        "name": "Logan Clark",
+        "number": "3579279199715359",
+        "cvv": "224",
+        "expiry": "12/26"
+    },
+    {
+        "type": "MasterCard",
+        "name": "John Roberts",
+        "number": "5255178941224809",
+        "cvv": "811",
+        "expiry": "12/27"
+    },
+    {
+        "type": "JCB",
+        "name": "Gabriel Bailey",
+        "number": "3596437978283725",
+        "cvv": "547",
+        "expiry": "12/27"
+    },
+    {
+        "type": "Visa",
+        "name": "Sofia Wilson",
+        "number": "4862111062673421",
+        "cvv": "586",
+        "expiry": "12/29"
+    },
+    {
+        "type": "Visa",
+        "name": "Tyler Jackson",
+        "number": "4796002561004308",
+        "cvv": "260",
+        "expiry": "12/26"
+    },
+    {
+        "type": "JCB",
+        "name": "Ethan Reed",
+        "number": "3588564699280517",
+        "cvv": "506",
+        "expiry": "12/30"
+    },
+    {
+        "type": "American Express",
+        "name": "Grace Phillips",
+        "number": "348291364754223",
+        "cvv": "3072",
+        "expiry": "12/27"
+    }
+];
