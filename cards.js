6f232ae9b1293f3b7823ae1f600365530c38fb88
jacem1 <mohamed.jacem@gmail.com>
2025-12-05 09:34:27 +0100

    Add multiple new card entries to cardData

--- cards.js
+++ cards.js
@@ -1,72 +1,282 @@
 const cardData = [
-    {
-        "type": "Visa",
-        "name": "James Reed",
-        "number": "4195462320425898",
-        "cvv": "903",
-        "expiry": "12/27"
-    },
-    {
-        "type": "JCB",
-        "name": "Riley Miller",
-        "number": "3523272171732646",
-        "cvv": "696",
-        "expiry": "12/27"
-    },
-    {
-        "type": "Visa",
-        "name": "Hannah Clark",
-        "number": "4967782302263002",
-        "cvv": "773",
-        "expiry": "12/29"
-    },
-    {
-        "type": "JCB",
-        "name": "Logan Clark",
-        "number": "3579279199715359",
-        "cvv": "224",
-        "expiry": "12/26"
-    },
-    {
-        "type": "MasterCard",
-        "name": "John Roberts",
-        "number": "5255178941224809",
-        "cvv": "811",
-        "expiry": "12/27"
-    },
-    {
-        "type": "JCB",
-        "name": "Gabriel Bailey",
-        "number": "3596437978283725",
-        "cvv": "547",
-        "expiry": "12/27"
-    },
-    {
-        "type": "Visa",
-        "name": "Sofia Wilson",
-        "number": "4862111062673421",
-        "cvv": "586",
-        "expiry": "12/29"
-    },
-    {
-        "type": "Visa",
-        "name": "Tyler Jackson",
-        "number": "4796002561004308",
-        "cvv": "260",
-        "expiry": "12/26"
-    },
-    {
-        "type": "JCB",
-        "name": "Ethan Reed",
-        "number": "3588564699280517",
-        "cvv": "506",
-        "expiry": "12/30"
-    },
-    {
-        "type": "American Express",
-        "name": "Grace Phillips",
-        "number": "348291364754223",
-        "cvv": "3072",
-        "expiry": "12/27"
-    }
+  {
+    "type": "Visa",
+    "name": "Penelope White",
+    "number": "4234407002490780",
+    "cvv": "701",
+    "expiry": "12/28"
+  },
+  {
+    "type": "MasterCard",
+    "name": "Aria Harris",
+    "number": "5259022607170971",
+    "cvv": "749",
+    "expiry": "12/26"
+  },
+  {
+    "type": "JCB",
+    "name": "Harper Hall",
+    "number": "3565819617063960",
+    "cvv": "842",
+    "expiry": "12/27"
+  },
+  {
+    "type": "Visa",
+    "name": "Jane Peterson",
+    "number": "4348707744999751",
+    "cvv": "208",
+    "expiry": "12/26"
+  },
+  {
+    "type": "MasterCard",
+    "name": "Aria Turner",
+    "number": "5183586074889707",
+    "cvv": "458",
+    "expiry": "12/30"
+  },
+  {
+    "type": "American Express",
+    "name": "James White",
+    "number": "346636055203794",
+    "cvv": "4033",
+    "expiry": "12/28"
+  },
+  {
+    "type": "Visa",
+    "name": "Maria Peterson",
+    "number": "4998447115959296",
+    "cvv": "728",
+    "expiry": "12/28"
+  },
+  {
+    "type": "American Express",
+    "name": "Jack Hall",
+    "number": "372909309221502",
+    "cvv": "5165",
+    "expiry": "12/29"
+  },
+  {
+    "type": "American Express",
+    "name": "Anthony Lewis",
+    "number": "344624681465487",
+    "cvv": "8240",
+    "expiry": "12/26"
+  },
+  {
+    "type": "American Express",
+    "name": "William Cook",
+    "number": "374407638013046",
+    "cvv": "5081",
+    "expiry": "12/30"
+  },
+  {
+    "type": "American Express",
+    "name": "Ellie Hill",
+    "number": "347601525616377",
+    "cvv": "0587",
+    "expiry": "12/26"
+  },
+  {
+    "type": "American Express",
+    "name": "Ethan Evans",
+    "number": "370945823631212",
+    "cvv": "3287",
+    "expiry": "12/27"
+  },
+  {
+    "type": "American Express",
+    "name": "Joshua Smith",
+    "number": "373346485390906",
+    "cvv": "7319",
+    "expiry": "12/28"
+  },
+  {
+    "type": "Visa",
+    "name": "Jack Ward",
+    "number": "4242396264894329",
+    "cvv": "023",
+    "expiry": "12/27"
+  },
+  {
+    "type": "MasterCard",
+    "name": "Maria Green",
+    "number": "5584685013910084",
+    "cvv": "649",
+    "expiry": "12/26"
+  },
+  {
+    "type": "American Express",
+    "name": "Ethan Jackson",
+    "number": "343140300769402",
+    "cvv": "7041",
+    "expiry": "12/30"
+  },
+  {
+    "type": "Visa",
+    "name": "Jane Turner",
+    "number": "4470764209296784",
+    "cvv": "974",
+    "expiry": "12/27"
+  },
+  {
+    "type": "JCB",
+    "name": "Isabella Robinson",
+    "number": "3546088426609195",
+    "cvv": "416",
+    "expiry": "12/30"
+  },
+  {
+    "type": "JCB",
+    "name": "Camila Richardson",
+    "number": "3509722402347219",
+    "cvv": "279",
+    "expiry": "12/29"
+  },
+  {
+    "type": "MasterCard",
+    "name": "Nicholas Jackson",
+    "number": "5412231830105356",
+    "cvv": "570",
+    "expiry": "12/27"
+  },
+  {
+    "type": "JCB",
+    "name": "Samuel Young",
+    "number": "3559387749770620",
+    "cvv": "085",
+    "expiry": "12/30"
+  },
+  {
+    "type": "Visa",
+    "name": "Jessica Watson",
+    "number": "4483194634087119",
+    "cvv": "515",
+    "expiry": "12/29"
+  },
+  {
+    "type": "JCB",
+    "name": "Joshua Evans",
+    "number": "3562657134344148",
+    "cvv": "806",
+    "expiry": "12/26"
+  },
+  {
+    "type": "American Express",
+    "name": "Layla Hall",
+    "number": "344469050511085",
+    "cvv": "0778",
+    "expiry": "12/26"
+  },
+  {
+    "type": "American Express",
+    "name": "Aaron Evans",
+    "number": "343309086224252",
+    "cvv": "8345",
+    "expiry": "12/27"
+  },
+  {
+    "type": "JCB",
+    "name": "Christian Gomez",
+    "number": "3504134067932580",
+    "cvv": "998",
+    "expiry": "12/27"
+  },
+  {
+    "type": "Visa",
+    "name": "Harper Gomez",
+    "number": "4056909780750054",
+    "cvv": "971",
+    "expiry": "12/27"
+  },
+  {
+    "type": "American Express",
+    "name": "John Baker",
+    "number": "373783800702419",
+    "cvv": "6559",
+    "expiry": "12/30"
+  },
+  {
+    "type": "Visa",
+    "name": "Samuel Miller",
+    "number": "4290680530659420",
+    "cvv": "279",
+    "expiry": "12/26"
+  },
+  {
+    "type": "MasterCard",
+    "name": "Paisley Edwards",
+    "number": "5462365677080745",
+    "cvv": "029",
+    "expiry": "12/27"
+  },
+  {
+    "type": "MasterCard",
+    "name": "Ellie Parker",
+    "number": "5143357636509367",
+    "cvv": "440",
+    "expiry": "12/28"
+  },
+  {
+    "type": "JCB",
+    "name": "Anthony Jackson",
+    "number": "3571259472408647",
+    "cvv": "488",
+    "expiry": "12/30"
+  },
+  {
+    "type": "MasterCard",
+    "name": "Samuel Richardson",
+    "number": "5587179245281267",
+    "cvv": "288",
+    "expiry": "12/28"
+  },
+  {
+    "type": "JCB",
+    "name": "Harper Morris",
+    "number": "3580107257475100",
+    "cvv": "389",
+    "expiry": "12/26"
+  },
+  {
+    "type": "MasterCard",
+    "name": "Robert Garcia",
+    "number": "5362219466832664",
+    "cvv": "587",
+    "expiry": "12/29"
+  },
+  {
+    "type": "JCB",
+    "name": "Layla Smith",
+    "number": "3564627884060893",
+    "cvv": "689",
+    "expiry": "12/26"
+  },
+  {
+    "type": "American Express",
+    "name": "David Allen",
+    "number": "372698215803441",
+    "cvv": "7903",
+    "expiry": "12/28"
+  },
+  {
+    "type": "MasterCard",
+    "name": "Andrew Lopez",
+    "number": "5573501861650557",
+    "cvv": "202",
+    "expiry": "12/29"
+  },
+  {
+    "type": "Visa",
+    "name": "Amelia Lee",
+    "number": "4532826360871941",
+    "cvv": "018",
+    "expiry": "12/28"
+  },
+  {
+    "type": "JCB",
+    "name": "Stella Green",
+    "number": "3569140134122271",
+    "cvv": "751",
+    "expiry": "12/29"
+  }
 ];
